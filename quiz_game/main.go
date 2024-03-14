9ae8c775ba1dc756b1578fd429be486c96345b86
zyeap <zachary.yeap1@gmail.com>
2024-03-14 12:26:33 -0700

    finished part 1 quiz game

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strconv"
 )
 
 // 1. read quiz file in CSV format (default to problems.csv)
@@ -14,7 +16,12 @@ import (
 // 4. output total number of Qs user got correct, and total number of Qs
 
 func main() {
-	f, err := os.Open("problems.csv")
+
+	var quiz_file = flag.String("f", "problems.csv", "file name containing quiz game, defaults to problems.csv")
+
+	flag.Parse()
+
+	f, err := os.Open(*quiz_file)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,6 +29,10 @@ func main() {
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
+
+	var correct int
+	var total int
+
 	for {
 		rec, err := csvReader.Read()
 		if err == io.EOF {
@@ -31,7 +42,25 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%+v\n", rec[0])
+
+		total++
+		fmt.Println(rec[0])
+		fmt.Print("Answer: ")
+
+		var answer int
+		fmt.Scan(&answer)
+
+		solution, err := strconv.Atoi(rec[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if answer == solution {
+			correct++
+		}
 	}
+
+	fmt.Printf("Num correct: %d\n", correct)
+	fmt.Printf("Total questions: %d\n", total)
 
 }
